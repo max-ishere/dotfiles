c39595a273311c153707d62ca4b9e0383ea2f34e
max-mkr <max.k.uakh@gmail.com>
2021-12-27 16:28:43 +0200

    Added DOOM Emacs config

---
+++ .doom.d/custom.el
@@ -0,0 +1,57 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#3c4c55" "#DF8C8C" "#A8CE93" "#DADA93" "#83AFE5" "#D18EC2" "#7FC1CA" "#c5d4dd"])
+ '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(exwm-floating-border-color "#4f5d65")
+ '(fci-rule-color "#556873")
+ '(highlight-tail-colors ((("#46595b") . 0) (("#425760") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d0f11" "#7FC1CA"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d0f11" "#A8CE93"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d0f11" "#899BA6"))
+ '(objed-cursor-color "#DF8C8C")
+ '(org-babel-do-load-languages 'org-babel-load-languages t)
+ '(org-plantuml-jar-path
+   (expand-file-name "/home/mkr/.local/bin/plantuml-newest.jar"))
+ '(pdf-view-midnight-colors (cons "#c5d4dd" "#3c4c55"))
+ '(rustic-ansi-faces
+   ["#3c4c55" "#DF8C8C" "#A8CE93" "#DADA93" "#83AFE5" "#D18EC2" "#7FC1CA" "#c5d4dd"])
+ '(safe-local-variable-values
+   '((projectile-switch-project-action . projectile-dired)
+     (projectile-switch-project-action quote projectile-dired)
+     (projectile-project-run-cmd . "cmake --build build -j 4 && ./build/src/sand_main")))
+ '(vc-annotate-background "#3c4c55")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A8CE93")
+    (cons 40 "#b8d293")
+    (cons 60 "#c9d693")
+    (cons 80 "#DADA93")
+    (cons 100 "#e2d291")
+    (cons 120 "#eaca90")
+    (cons 140 "#F2C38F")
+    (cons 160 "#e7b1a0")
+    (cons 180 "#dc9fb1")
+    (cons 200 "#D18EC2")
+    (cons 220 "#d58db0")
+    (cons 240 "#da8c9e")
+    (cons 260 "#DF8C8C")
+    (cons 280 "#c98f92")
+    (cons 300 "#b39399")
+    (cons 320 "#9e979f")
+    (cons 340 "#556873")
+    (cons 360 "#556873")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
